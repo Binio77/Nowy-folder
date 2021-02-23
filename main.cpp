4133656668222099dd111bd5da45218759281aef
Artur Binczyk <binczykartur77@gmail.com>
2021-02-23 19:40:35 +0100

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<fstream>
+
+using namespace std;
+
+void bubble(int tab[])
+{
+
+}
+
+int main()
+{
+    ifstream f_wyj ("wyjscie.txt");
+    int tab1[200], tab2[500], tab3[1000], tab4[10000], tab5[100000];
+    bubble(tab1);
+    getchar();
+    getchar();
+    return 0;
+}
